ec7b8bc854e7fabdaaec016785df960be2306ff7
John Wallace <johnwallace123@gmail.com>
2016-10-17 13:53:45 -0400

    Script tweaks for consistency
    - Setting the project during start of execution

--- scripts/parallel_upload.py
+++ scripts/parallel_upload.py
@@ -7,16 +7,17 @@ import dxpy
 
 
 class Uploader(object):
-	def __init__(self, basedir, rempath=""):
+	def __init__(self, basedir, proj, rempath=""):
 		self.basedir = basedir
 		self.rempath = rempath
+		self.proj = proj
 	
 	def __call__(self, fullf):
 		folder_str=self.rempath + "/" + '/'.join((fullf[len(self.basedir):] if fullf.startswith(self.basedir) else fullf).split(os.path.sep)[:-1])
 		folder_str=("/" + folder_str if not folder_str.startswith("/") else folder_str)
 	
 		print >> sys.stderr, "Uploading " + fullf + " to " + folder_str
-		dxpy.bindings.dxfile_functions.upload_local_file(filename=fullf, folder=folder_str, parents=True)
+		dxpy.bindings.dxfile_functions.upload_local_file(filename=fullf, folder=folder_str, parents=True, project=self.proj)
 		print >> sys.stderr, "Finished uploading "+ fullf
 
 if __name__ == "__main__":
@@ -24,6 +25,7 @@ if __name__ == "__main__":
 	upload_path="."
 	dx_path=""
 	remote_path="/"
+	curr_proj = dxpy.PROJECT_CONTEXT_ID
 	# try to find the remote path
 	try:
 		with open(os.path.join(os.path.expanduser("~"), ".dnanexus_config", "DX_CLI_WD")) as f:
@@ -47,4 +49,4 @@ if __name__ == "__main__":
 			
 	
 	p = multiprocessing.Pool(multiprocessing.cpu_count())
-	p.map(Uploader(upload_path, remote_path), pathlist)
+	p.map(Uploader(upload_path, curr_proj, remote_path), pathlist)

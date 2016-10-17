d6bd234ce579f4389f5a060043bd59edc14e952f
John Wallace <johnwallace123@gmail.com>
2016-10-17 13:44:01 -0400

    Made parallel uploading better
    - Added an option to specify the upload path
    - Made the upload path accept the current CLI WD, if it exists
    - Fall back to old behavior of uploading to the root.

--- scripts/parallel_upload.py
+++ scripts/parallel_upload.py
@@ -7,21 +7,38 @@ import dxpy
 
 
 class Uploader(object):
-	def __init__(self, basedir):
+	def __init__(self, basedir, rempath=""):
 		self.basedir = basedir
+		self.rempath = rempath
 	
 	def __call__(self, fullf):
-		folder_str='/'.join((fullf[len(self.basedir):] if fullf.startswith(self.basedir) else fullf).split(os.path.sep)[:-1])
+		folder_str=self.rempath + "/" + '/'.join((fullf[len(self.basedir):] if fullf.startswith(self.basedir) else fullf).split(os.path.sep)[:-1])
 		folder_str=("/" + folder_str if not folder_str.startswith("/") else folder_str)
 	
+		print >> sys.stderr, "Uploading " + fullf + " to " + folder_str
 		dxpy.bindings.dxfile_functions.upload_local_file(filename=fullf, folder=folder_str, parents=True)
+		print >> sys.stderr, "Finished uploading "+ fullf
 
 if __name__ == "__main__":
 
 	upload_path="."
 	dx_path=""
+	remote_path="/"
+	# try to find the remote path
+	try:
+		with open(os.path.join(os.path.expanduser("~"), ".dnanexus_config", "DX_CLI_WD")) as f:
+			remote_path=f.read()
+	except:
+		pass
+	
 	if len(sys.argv) > 1:
 		upload_path=sys.argv[1]
+	if len(sys.argv) > 2:
+		given_rpath=sys.argv[2]
+		if given_rpath.startswith("/"):
+			remote_path=given_rpath
+		else:
+			remote_path = remote_path + "/" + given_rpath
 		
 	pathlist = []
 	for r,d,fs in os.walk(upload_path):
@@ -30,4 +47,4 @@ if __name__ == "__main__":
 			
 	
 	p = multiprocessing.Pool(multiprocessing.cpu_count())
-	p.map(Uploader(upload_path), pathlist)
+	p.map(Uploader(upload_path, remote_path), pathlist)
